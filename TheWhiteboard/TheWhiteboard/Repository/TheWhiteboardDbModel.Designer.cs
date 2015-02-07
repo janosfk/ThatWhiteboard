f39888ac3fe4641b7812f2c254272e660c83cf7d
János Fodor Kis <dotnetjanos@hotmail.com>
2015-02-07 16:49:09 -0600

    fixing registration

--- TheWhiteboard/TheWhiteboard/Repository/TheWhiteboardDbModel.Designer.cs
+++ TheWhiteboard/TheWhiteboard/Repository/TheWhiteboardDbModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\GitHub\ThatWhiteboard\TheWhiteboard\TheWhiteboard\Repository\TheWhiteboardDbModel.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\gh\ThatWhiteboard\TheWhiteboard\TheWhiteboard\Repository\TheWhiteboardDbModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
